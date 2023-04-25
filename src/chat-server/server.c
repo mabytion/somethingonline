33b7be34634e14b511a889cb595e6f6f612c082b
mabytion <mabytion@live.com>
2023-04-26 02:25:12 +0900

    classification

---
+++ src/chat-server/server.c
@@ -0,0 +1,243 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  server_epoll.c
+ *
+ *    Description:  epoll
+ *
+ *        Version:  1.0
+ *        Created:  2023년 04월 21일 16시 27분 02초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define MAX_CLIENT 100
+#define BUFF_SIZE 1024
+#define LOG_SIZE 1536
+
+struct udata
+{
+	int fd;
+	char name[80];
+};
+
+int user_fds[SOMAXCONN];
+
+void send_msg(struct epoll_event ev, char* msg);
+
+int main()
+{
+	struct sockaddr_in serv_sock, client_sock;
+	struct sockaddr_in log_sock;
+	struct epoll_event* events;	
+	struct epoll_event ev;
+
+	int listen_fd = 0;
+	int client_fd = 0;
+	int log_fd = 0;
+	int ret = 0;
+	int epoll_fd = 0;
+	int client_fd_size = 0;
+
+	struct udata* user_data;
+
+	char buf[BUFF_SIZE+1];
+	char tmp[LOG_SIZE];
+	size_t read_size;
+	int i;
+
+	events = (struct epoll_event*)malloc(sizeof(struct epoll_event) * SOMAXCONN);
+	/* to log server */
+	if((log_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		printf("create log socket fail\n");
+		return -1;
+	}
+
+	memset(&log_sock, 0, sizeof(log_sock));
+	log_sock.sin_family		= AF_INET;
+	log_sock.sin_port		= htons(4001);
+	log_sock.sin_addr.s_addr	= inet_addr("192.168.75.11");
+
+	if(connect(log_fd, (struct sockaddr*)&log_sock, sizeof(log_sock)) < 0)
+	{
+		printf("connect err\n");
+		fprintf(stderr, "%s\n", strerror(errno));
+	}
+
+
+	/* serv sock setting */
+	if((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		printf("create socket fail\n");
+		return -1;
+	}
+
+	memset(&serv_sock, 0, sizeof(serv_sock));
+	serv_sock.sin_family		= AF_INET;
+	serv_sock.sin_port		= htons(4000);
+	serv_sock.sin_addr.s_addr	= htonl(INADDR_ANY);
+
+	if(bind(listen_fd, (struct sockaddr*)&serv_sock, sizeof(serv_sock)) < 0)
+	{
+		printf("bind fail\n");
+		return -1;
+	}
+			
+	if(listen(listen_fd, 5))
+	{
+		printf("listen fail\n");
+		exit(1);
+	}
+
+	/* epoll create */
+	if((epoll_fd = epoll_create(SOMAXCONN)) < 0)
+	{
+		printf("epoll_create() fail\n");
+		return -1;
+	}
+
+	/* epoll setting */
+	ev.events = EPOLLIN;
+	ev.data.fd = epoll_fd;
+
+	/* epoll add */
+	epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_fd, &ev);
+	memset(user_fds, -1, sizeof(int) * SOMAXCONN);
+
+	while(1)
+	{
+		/* epoll wait */
+		ret = epoll_wait(epoll_fd, events, SOMAXCONN, -1);
+
+		if(ret < -1)
+		{
+			printf("epoll_wait\n");
+			break;
+		}
+
+		else if(ret == 0)
+		{
+			printf("idle\n");
+			continue;
+		}
+		
+		else
+		{
+			/* fd work */
+			for(i=0;i<ret;i++)
+			{	
+				if(events[i].data.fd == epoll_fd)
+				{
+					memset(buf, 0, BUFF_SIZE+1);
+					client_fd_size = sizeof(client_sock);
+					client_fd = accept(listen_fd, (struct sockaddr*)&client_sock, &client_fd_size);
+					read(client_fd, buf, BUFF_SIZE+1);
+					if(strncmp(buf, "ok", 2) == 0)
+					{
+						write(client_fd, "ok\n", 3);
+						user_fds[client_fd] = 1;
+		
+						user_data = malloc(sizeof(user_data));
+						user_data->fd = client_fd;
+		
+						write(user_data->fd, "your name: ", 29);
+		
+						sleep(1);
+						read(user_data->fd, user_data->name, sizeof(user_data->name));
+						user_data->name[strlen(user_data->name)-1] = 0;
+		
+						printf("yeah [%s] \n", user_data->name);
+		
+						sleep(1);
+		
+						sprintf(buf, "your name: %s\n", user_data->name);
+	
+						write(user_data->fd, buf, 40);
+		
+						ev.events = EPOLLIN;
+						ev.data.ptr = user_data;
+		
+						epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &ev);
+					}
+					else
+					{
+						printf("close fd: %d\n",  client_fd);
+						close(client_fd);
+					}
+				}
+				else
+				{
+					user_data = events[i].data.ptr;
+					memset(buf, 0, BUFF_SIZE+1);
+					read_size = read(user_data->fd, buf, BUFF_SIZE+1);
+					memset(tmp, 0, LOG_SIZE);
+					sprintf(tmp, "logging >> {%s}.{%s}", user_data->name, buf);
+					write(log_fd, tmp, sizeof(tmp));
+					if(read_size <= 0)
+					{
+						epoll_ctl(epoll_fd, EPOLL_CTL_DEL, user_data->fd, events);
+						close(user_data->fd);
+						printf("close fd: %d\n", user_data->fd);
+						printf("Connection lost: %s\n", user_data->name);
+						user_fds[user_data->fd] = -1;
+						free(user_data);
+					}
+					else
+					{
+						send_msg(events[i], buf);
+					}
+					
+				}
+			}
+	
+			continue;
+		}
+	}
+
+	if(close(epoll_fd))
+	{
+		fprintf(stderr, "Failed to close epoll fd\n");
+		return 1;
+	}
+	
+
+	return 0;
+}
+
+void send_msg(struct epoll_event ev, char* msg)
+{
+	int i;
+	char buf[BUFF_SIZE + 24];
+	struct udata* user_data;
+	user_data = ev.data.ptr;
+	for(i=0;i<SOMAXCONN;i++)
+	{
+		memset(buf, 0, BUFF_SIZE + 24);
+		sprintf(buf, "%s : %s", user_data->name, msg);
+		if(user_fds[i] == 1)
+		{
+			write(i, buf, BUFF_SIZE + 24);
+		}
+	}
+}
---
+++ src/testkit/c/log_tcp_client.c
@@ -0,0 +1,68 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  tcp_client.c
+ *
+ *    Description:  TCP Client Example
+ *
+ *        Version:  1.0
+ *        Created:  01/17/2023 05:41:26 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define BUFF_SIZE 1024
+
+int main(int argc, char** argv)
+{
+	int client_socket;
+	struct sockaddr_in server_addr;
+	char buff[BUFF_SIZE + 5];
+
+	int i;
+
+	client_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if(client_socket < 0)
+	{
+		printf("socket err: ln 35\n");
+		exit(1);
+	}
+
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family		= AF_INET;
+	server_addr.sin_port		= htons(4001);	
+	server_addr.sin_addr.s_addr	= inet_addr("192.168.75.11");
+
+	if(connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)))
+	{
+		printf("connect err: ln 47\n");
+		exit(1);
+	}
+	
+	char* tmp = "logging";
+
+	for(i=0;i<10;i++)
+	{
+		write(client_socket, tmp, strlen(tmp));
+		printf("%s\n", tmp);
+		sleep(1);
+	}
+	
+	write(client_socket, "exit", 4);
+	close(client_socket);
+	
+	return 0;
+}
