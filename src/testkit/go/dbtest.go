2c1e807c8eadc8aa0f618e0d7f982dfadf4ef16a
mabytion <mabytion@live.com>
2023-04-27 03:42:50 +0900

    MongoDB Connection

--- src/testkit/go/dbtest.go
+++ src/testkit/go/dbtest.go
@@ -2,17 +2,23 @@ package main
 
 import (
 		"context"
-		"encoding/json"
-		"fmt"
 		"log"
+		"time"
 		"os"
 
-		"go.mongodb.org/mongo-driver/bson"
+		"github.com/joho/godotenv"
 		"go.mongodb.org/mongo-driver/mongo"
 		"go.mongodb.org/mongo-driver/mongo/options"
        )
 
-// const uri = "mongodb://user:root@192.168.75.11:27017/?maxPoolSize=20&w=majority"
+
+type Logdata struct {
+	Date_time	string
+	Server		string
+	Category	string
+	Id		string
+	Data		strig
+}
 
 func main() {
 	if err := godotenv.Load(); err != nil {
@@ -24,4 +30,37 @@ func main() {
 	if uri == "" {
 		log.Fatal("You must set your 'MONGODB_URI' environmental variable.")
 	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	dt := time.Now()
+	log.Println(dt)
+
+	coll := client.Database("log").Collection("log")
+	newLogdata := Logdata{
+				Date_time: dt.Format("2006-01-02T15:04:05Z"),
+				Server: "test",
+				Category: "test",
+				Id: "test",
+				Data: "test",
+			     }
+
+	log.Println(newLogdata);
+	result, err := coll.InsertOne(context.TODO(), newLogdata)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(result);
+
+
 }
+
